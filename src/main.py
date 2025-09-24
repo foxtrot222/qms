ecdbe87aa35c40f2f025d97073290d0413aa593c
BHATIYA KAVYA BHAVIN <kavyabhatiya3019@gmail.com>
2025-09-24 21:05:36 +0530

    feat: setup connection between Flask and MySQL

--- src/main.py
+++ src/main.py
@@ -1,10 +1,21 @@
 from flask import Flask, render_template
-
+import mysql.connector
 app = Flask(__name__)
+try:
+    con=mysql.connector.connect(host='localhost',
+                                user='root',
+                                password='your_password',
+                                database='QMS')
+    print("Database connected successfully!")
 
+except mysql.connector.Error as e:
+    print("Database connection failed:", e)
+    con = None
 @app.route("/")
-def home():
-    return render_template("index.html")
 
+#Home Page
+def home():
+    cur=con.cursor()
+    return render_template("index.html")
 if __name__ == "__main__":
     app.run(debug=True)

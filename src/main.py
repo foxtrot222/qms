849a0ba11ffe1a6354d9ff2f6b401fb3aac473e9
BHATIYA KAVYA BHAVIN <kavyabhatiya3019@gmail.com>
2025-09-25 15:12:26 +0530

    feat: add environment feature

--- src/main.py
+++ src/main.py
@@ -1,22 +1,26 @@
 from flask import Flask, render_template
+from dotenv import load_dotenv
+import os
 import mysql.connector
-
+# Load the .env file
+load_dotenv()
 app = Flask(__name__)
+# Access values from .env
+app.secret_key = os.getenv("SECRET_KEY")
 try:
-    con=mysql.connector.connect(host='localhost',
-                                user='root',
-                                password='Tirth@2006',
-                                database='QMS')
-    print("Database connected successfully!")
-
+    conn = mysql.connector.connect(
+        host="localhost",
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASS"),
+        database=os.getenv("DB_NAME")
+    )
+    print('Database connected successfuly.')
 except mysql.connector.Error as e:
-    print("Database connection failed:", e)
-    con = None
-
+    print(f"Database connection failed: {e}")
+    conn = None  # Avoid crashing if DB is not connected
 @app.route("/")
+#Home Page
 def home():
-    cur=con.cursor()
     return render_template("index.html")
-
 if __name__ == "__main__":
     app.run(debug=True)

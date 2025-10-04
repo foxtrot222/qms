cb0e160c7b5e7055210ebefa2d890f0c37b387cd
Tirth Kavathiya <93577697+foxtrot222@users.noreply.github.com>
2025-10-04 19:03:18 +0530

    feat: update host from local to cloud

--- src/main.py
+++ src/main.py
@@ -22,10 +22,10 @@ def get_db_connection():
     """Establishes a database connection."""
     try:
         conn = mysql.connector.connect(
-            host="localhost",
-            user=os.getenv("DB_USER"),
-            password=os.getenv("DB_PASS"),
-            database=os.getenv("DB_NAME")
+            host=os.getenv("DB_HOST"),  
+            user=os.getenv("DB_USER"),  
+            password=os.getenv("DB_PASS"),  
+            database=os.getenv("DB_NAME")
         )
         return conn
     except mysql.connector.Error as e:

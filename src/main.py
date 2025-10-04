9abe0eef0d6e917fa2209dc9fd548b4af50b14a1
Tirth Kavathiya <93577697+foxtrot222@users.noreply.github.com>
2025-10-04 19:50:48 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -23,9 +23,9 @@ def get_db_connection():
     try:
         conn = mysql.connector.connect(
             host=os.getenv("DB_HOST"),
-            user=os.getenv("DB_USER"),
-            password=os.getenv("DB_PASS"),
-            database=os.getenv("DB_NAME")
+            user=os.getenv("DB_USER"),
+            password=os.getenv("DB_PASS"),
+            database=os.getenv("DB_NAME")
         )
         return conn
     except mysql.connector.Error as e:

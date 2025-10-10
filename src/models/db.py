b7fab0aad45c6043cb3239901699adfba7b2740f
foxtrot222 <tirthkavathiya@gmail.com>
2025-10-10 23:35:47 +0530

    fix: password to pass

--- src/models/db.py
+++ src/models/db.py
@@ -6,7 +6,7 @@ def get_db_connection():
         conn = mysql.connector.connect(
             host=os.getenv("DB_HOST", "127.0.0.1"),  # Use TCP/IP, not named pipe
             user=os.getenv("DB_USER", "root"),
-            password=os.getenv("DB_PASSWORD", ""),
+            password=os.getenv("DB_PASS", ""),
             database=os.getenv("DB_NAME", "qms_db"),
             port=int(os.getenv("DB_PORT", 3306)),     # Ensure port is included
             unix_socket=None,                         # Disable named pipes
